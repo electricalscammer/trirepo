2420f5983583404b3792830b60f18880d53054ea
electricalscammer <155905329+electricalscammer@users.noreply.github.com>
2024-01-09 22:03:29 +0530

    Update p_bar.py

--- p_bar.py
+++ p_bar.py
@@ -14,7 +14,7 @@ class Timer:
             self.start_time = time.time()
             return True
         return False
-# 𝙋𝙤𝙬𝙚𝙧𝙚𝙙 𝘽𝙮 🅰🅸🆁 🅿🅷🅴🅾🅽🅸🆇
+# 𝙋𝙤𝙬𝙚𝙧𝙚𝙙 𝘽𝙮 Electric Hacker 
 timer = Timer()
 async def progress_bar(current,total,reply,start):
       if timer.can_send():
@@ -38,10 +38,10 @@ async def progress_bar(current,total,reply,start):
             bar_length = 11
             completed_length = int(current * bar_length / total)
             remaining_length = bar_length - completed_length
-            progress_bar = "■" * completed_length + "□" * remaining_length
+            progress_bar = "▰" * completed_length + "▱" * remaining_length
             
             try:
-                await reply.edit(f'╭──⌈📤 𝙐𝙥𝙡𝙤𝙖𝙙𝙞𝙣𝙜 📤⌋──╮ \n├{progress_bar}\n├ 𝙎𝙥𝙚𝙚𝙙⚡ : {sp} \n├ 𝙋𝙧𝙤𝙜𝙧𝙚𝙨𝙨🧭 : {perc} \n├ 𝙇𝙤𝙖𝙙𝙚𝙙🗂️ : {cur}\n├ 𝙎𝙞𝙯𝙚📭 :  {tot} \n├ 𝙀𝙏𝘼⏳: {eta} \n╰────⌈S A K S H A M⌋───╯\n') 
+                await reply.edit(f'╭──⌈🚔 𝙐𝙥𝙡𝙤𝙖𝙙𝙞𝙣𝙜 🚔⌋──╮ \n├{progress_bar}\n├ 𝙎𝙥𝙚𝙚𝙙⚙️ : {sp} \n├ 𝙋𝙧𝙤𝙜𝙧𝙚𝙨𝙨🧭 : {perc} \n├ 𝙇𝙤𝙖𝙙𝙚𝙙📂 : {cur}\n├ 𝙎𝙞𝙯𝙚🛰 :  {tot} \n├ 𝙀𝙏𝘼🎡: {eta} \n╰────⌈ Electric Hacker ⌋───╯\n') 
             except FloodWait as e:
                 time.sleep(e.x)
                 time.sleep(e.x)
